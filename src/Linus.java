06a48cfd7a1376716768833757cdb72a8f2f735a
Patrick Langer <patrick.langer@haw-hamburg.de>
2016-09-29 21:05:42 +0200

    @ensure und @require hinzugefügt

--- src/Linus.java
+++ src/Linus.java
@@ -7,29 +7,29 @@ public interface Linus {
 	 * Maximale Puffer groesse
 	 */
 	public final static int MAX_PUFFER = 10;
-	
+
 	/**
 	 * Fuegt einen neuen Wert am Ende des Puffers hinzu
-	 * VB: value.length > 0
-	 * VB: size() < 10
-	 * NB: contains(String value) == true
-	 * NB: !isEmpty()
-	 * NB: Festhalten, dass sich die groesse des Puffers aendert?
+	 * @require value.length > 0
+	 * @require size() < 10
+	 * @ensure contains(String value) == true
+	 * @ensure !isEmpty()
+	 * @ensure Festhalten, dass sich die groesse des Puffers aendert?
 	 * @param value 	Der Prozess, der gepuffert werden soll.
 	 * @return    		true, wenn erfolgreich. false, wenn erfolglos.
 	 */
 	public abstract boolean push(String value);
-	
+
 	/**
 	 * Entnimmt erstes Objekt aus Puffer
-	 * VB: size() > 0
-	 * NB: String != null
+	 * @require size() > 0
+	 * @ensure String != null
 	 * @return 			Der naechste gepufferte Prozess.
 	 */
 	public abstract String pop();
 
 	//TODO brauchen wir noch eine Funktion wie pop ohne dass der Wert verschwindet?
-	
+
 	/**
 	 * Entfernt einen Werten von der angegeben Position des Puffers
 	 * TODO Wie sollen wir an die pos kommen?? Wird die Funktion benoetigt??
@@ -37,26 +37,26 @@ public interface Linus {
 	 * @return 			true, wenn erfolgreich. false, wenn erfolglos.
 	 */
 	public abstract boolean remove(int pos);
-	
+
 	/**
 	 * Prueft ob ein Wert im Puffer vorhanden ist.
-	 * NB: contains(String value) != null
+	 * @ensure contains(String value) != null
 	 * @param value 	Der Prozess, der gepuffert werden soll.
 	 * @return    		true, wenn vorhanden. false, wenn nicht vorhanden.
 	 */
 	public abstract boolean contains(String value);
-	
+
 	/**
 	 * Gibt die aktuelle Groesse des Puffers zurueck
-	 * NB: size() <= 10
-	 * NB: size() >= 0
+	 * @ensure size() <= 10
+	 * @ensure size() >= 0
 	 * @return 			Die aktuelle Groesse des Puffers.
 	 */
 	public abstract int size();
-	
+
 	/**
 	 * Prueft ob der Puffer leer ist
-	 * NB: isEmpty() != null
+	 * @ensure isEmpty() != null
 	 * @return    		true, wenn Puffer leer, also size()=0. false, wenn size()>0.
 	 */
 	public abstract boolean isEmpty();
