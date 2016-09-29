3b6ee85b01c795fa83839e9f54fbc82031a08b0e
Jeff aaa <Jeff aaa>
2016-09-29 21:24:58 +0200

    Add abstract Linus

---
+++ src/abstractLinus.java
@@ -0,0 +1,44 @@
+
+public abstract class abstractLinus implements Linus {
+	
+	/**
+	 * Maximale Puffer groesse
+	 */
+	public final static int MAX_PUFFER = 10;
+	
+	@Override
+	public boolean push(String value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(int pos) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(String value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
