d80ff8697cc8f76d83281c0967c4947f9d4c9f9d
soenke kracht <sk@nrpl.com>
2016-11-03 19:29:36 +0100

    6.1

--- ADPraktikum/src/Aufgabe_6/Generator.java
+++ ADPraktikum/src/Aufgabe_6/Generator.java
@@ -1,23 +1,54 @@
+package Aufgabe_6;
+
 import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class Generator {
-	private int keyLimit;
-	private double p;
-	private double q;
 
-	
-	
-	public Generator(int keyLimit, double p, double q) {
-		this.keyLimit = keyLimit;
-		this.p = p;
-		this.q = q;
+	private Generator() {
 	}
-	
-	public void create(Dictionary d, int n){
+
+	public static Dictionary<Integer, String> create(int keyLimit, double p, double q) {
+		Dictionary<Integer, String> d = new Hashtable<Integer, String>();
 		Random random = new Random();
-		
-		d.put(random.nextInt(keyLimit) + 1, random.nextInt());
-		if(Math.random() > )
+		LinkedList<Integer> keys = new LinkedList<Integer>();
+		Integer key;
+		int i;
+
+		boolean added = true;
+		key = random.nextInt(keyLimit) + 1;
+		d.put(key, Integer.toString(key));
+		keys.add(key);
+
+		while (added) {
+			added = false;
+
+			if (Math.random() <= p) {
+				key = random.nextInt(keyLimit) + 1;
+				d.put(key, Integer.toString(key));
+				keys.add(key);
+				added = true;
+			} else {
+				i = random.nextInt(keys.size());
+				d.remove(keys.get(i));
+				keys.remove(i);
+
+				if (Math.random() <= q) {
+					if (!d.isEmpty()) {
+						i = random.nextInt(keys.size());
+						d.remove(keys.get(i));
+						keys.remove(i);
+					}
+				} else {
+					key = random.nextInt(keyLimit) + 1;
+					d.put(key, Integer.toString(key));
+					keys.add(key);
+					added = true;
+				}
+			}
+		}
+		return d;
 	}
 }
---
+++ ADPraktikum/src/Aufgabe_6/main.java
@@ -0,0 +1,19 @@
+package Aufgabe_6;
+
+public class main {
+
+	public static void main(String[] args) {
+		int numberOfExpiriments = 100;
+		int akkumulatorSizes = 0;
+
+		double p = 0.9;
+		double q = 0.1;
+		for (int i = 0; i < numberOfExpiriments; i++) {
+			akkumulatorSizes += Generator.create(100000, 0.9, 0.1).size();
+		}
+		System.out.println("p = " + p + "   q = " + q + "   average size = "
+				+ ((float) akkumulatorSizes / (float) numberOfExpiriments) + "   number of expiriments = "
+				+ numberOfExpiriments);
+	}
+
+}
