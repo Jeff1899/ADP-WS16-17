90895624a9f0d9999cfe0fc087a9d287a045c67e
soenke kracht <sk@nrpl.com>
2016-11-11 09:24:31 +0100

    tst

--- ADPraktikum/src/Aufgabe_7/TestGauss.java
+++ ADPraktikum/src/Aufgabe_7/TestGauss.java
@@ -9,9 +9,9 @@ import org.junit.Test;
 public class TestGauss {
 
 	@Test
-	public void test() {
+	public void testRational() {
 		Random random = new Random();
-		int rows = 10;//random.nextInt(1000) + 1;
+		int rows = 10;// random.nextInt(1000) + 1;
 		int colums = rows + 1;
 
 		Rational[] res = new Rational[rows];
@@ -26,29 +26,68 @@ public class TestGauss {
 		for (int r = 0; r < rows; r++) {
 			matrix[r][r] = new Rational(1);
 
-			zaehler = (random.nextInt(100) + 1) * (random.nextInt(1) == 1 ? 1 : -1);
-			nenner = random.nextInt(100) + 1;
+			zaehler = (random.nextInt(10) + 1) * (random.nextInt(1) == 1 ? 1 : -1);
+			nenner = random.nextInt(10) + 1;
 
 			matrix[r][colums - 1] = new Rational(zaehler, nenner);
 			res[r] = new Rational(zaehler, nenner);
 		}
-		
+
 		MatrixOps.p(matrix);
 		System.out.println();
-		
+
 		for (int i = 0; i < 25; i++) {
-			MatrixOps.addRows(random.nextInt(rows), random.nextInt(rows), new Rational(
-					(random.nextInt(5) + 1) * (random.nextInt(1) == 1 ? 1 : -1), random.nextInt(5) + 1), matrix);
+			MatrixOps.addRows(random.nextInt(rows), random.nextInt(rows),
+					new Rational((random.nextInt(5) + 1) * (random.nextInt(1) == 1 ? 1 : -1), random.nextInt(5) + 1),
+					matrix);
 		}
 
 		MatrixOps.p(matrix);
 
 		Gauss.solve(matrix);
-		
 
 		for (int r = 0; r < rows; r++) {
 			assertTrue(matrix[r][colums - 1].sub(res[r]).naught());
 		}
 	}
 
+	@Test
+	public void testDouble() {
+		Random random = new Random();
+		int rows = 10;// random.nextInt(1000) + 1;
+		int colums = rows + 1;
+
+		double[] res = new double[rows];
+
+		double[][] matrix = new double[rows][colums];
+		for (int r = 0; r < rows; r++)
+			for (int c = 0; c < colums; c++)
+				matrix[r][c] = 0;
+
+		double n;
+		for (int r = 0; r < rows; r++) {
+			matrix[r][r] = 1;
+
+			n = ((double) random.nextInt(100) / 10.0) * (random.nextInt(1) == 1 ? 1 : -1);
+
+			matrix[r][colums - 1] = n;
+			res[r] = n;
+		}
+
+		// MatrixOps.p(matrix);
+		System.out.println();
+
+		for (int i = 0; i < 5; i++) {
+			MatrixOps.addRows(random.nextInt(rows), random.nextInt(rows),
+					((double) random.nextInt(10) + 1) * (random.nextInt(1) == 1 ? 1 : -1), matrix);
+		}
+
+		// MatrixOps.p(matrix);
+
+//		Gauss.solve(matrix);
+
+		for (int r = 0; r < rows; r++) {
+			assertTrue(matrix[r][colums - 1] == res[r]);
+		}
+	}
 }
